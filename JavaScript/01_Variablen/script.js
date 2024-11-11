46ac506cd2b12857243d1dbc2ab231142c322b09
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2024-11-11 15:46:46 +0100

    Started_with_JavaScript

---
+++ JavaScript/01_Variablen/script.js
@@ -0,0 +1,28 @@
+console.log("Hallo");
+console.log("Tolle Sache");
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+d = a + b + c
+console.log("a+b+c = " + d);
+
+d = (a + b) / c
+console.log("(a + b) / c = " + d);
+
+d = c -a + b
+console.log("c -a + b = " + d);
+
+d = a + b + c * 3
+console.log("a + b + c * 3 = " + d);
+
+d = (a + b + c)/4
+console.log("(a + b + c)/4 = " + d);
+
+a = 3
+console.log("a = " + a);
+b = a + 3
+console.log("b = " + b);
+c = c -3
+console.log("c = " + c);
