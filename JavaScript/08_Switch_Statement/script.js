47866ce2dbb3c551194cf4338933b3e6633539f8
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2024-11-15 09:40:55 +0100

    Finished_08_Switch_Statement

---
+++ JavaScript/08_Switch_Statement/script.js
@@ -0,0 +1,39 @@
+let randomNumber = Math.random() * (10-5+1);
+a = Math.floor(randomNumber)+5;
+
+console.log(a);
+
+switch(a) {
+    case 0:
+        console.log("Null");
+        break;
+    case 1:
+        console.log("Eins");
+        break;
+    case 2:
+        console.log("Zwei");
+        break;
+    case 3:
+        console.log("Drei");
+        break;
+    case 4:
+        console.log("Vier");
+        break;
+    case 5:
+        console.log("Fünf");
+        break;
+    case 6:
+        console.log("Sechs");
+        break;
+    case 7:
+        console.log("Sieben");
+        break;
+    case 8:
+        console.log("Acht");
+        break;
+    case 9:
+        console.log("Neun");
+        break;
+    case 10:
+        console.log("Zehn");
+}
