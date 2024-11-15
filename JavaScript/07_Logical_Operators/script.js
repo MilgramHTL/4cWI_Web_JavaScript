995abcfc1f2992324564ef5391089aedf96b7d88
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2024-11-15 09:27:04 +0100

    Finish_07_Logical_Operators

--- JavaScript/07_Logical_Operators/script.js
+++ JavaScript/07_Logical_Operators/script.js
@@ -1,7 +1,23 @@
-let randomNumber = Math.random() * 100;
-let a = Math.floor(randomNumber);
-let b = Math.floor(randomNumber);
+let randomNumbera = Math.random() * 100;
+let randomNumberb = Math.random() * 100;
+let a = Math.floor(randomNumbera);
+let b = Math.floor(randomNumberb);
+console.log(a);
+console.log(b);
+if ((a < b) && (a < 50)){
+    console.log("Zahl 1 ist kleiner als Zahl 2 und Mini");
+} else {
+    console.log("Zahl 2 ist größer");
+}
 
-if (a < b) && (a < 50){
+if ((a < 30) || (b < 30)){
+    console.log("Eine der beiden ist kleiner als 30)");
+} else {
+    console.log("Beide sind größer als 30");
+}
 
+if ((a < 50) && (b != 50)){
+    console.log("Erste Zahl klein, zweite kein 50iger");
+} else {
+    console.log("Erste Zahl zu groß, oder zweite ein 50iger");
 }
