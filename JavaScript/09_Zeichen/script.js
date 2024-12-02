f678bfe2e7f869ca31f448b2328f7764bdc17c8d
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2024-12-02 15:19:27 +0100

    Finished_Zeichen_09

--- JavaScript/09_Zeichen/script.js
+++ JavaScript/09_Zeichen/script.js
@@ -2,3 +2,20 @@ for (var i = 0; i <3; i++) {
     console.log("XXXXXXX")
     console.log("OOOOOOO")
 }
+let sum = 0;
+for (var i = 0; i <= 100; i++) {
+    if (i % 2 == 0) {
+        sum += i;
+    }
+}
+
+console.log(sum);
+
+for (let i = 1; i < 2; i ++) {
+    console.log("X     ");
+    console.log("X     ");
+    console.log("X     ");
+    console.log("X     ");
+    console.log("X     ");
+    console.log("XXXXXX");
+}
