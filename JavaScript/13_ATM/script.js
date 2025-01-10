216bc5ff08d2d54452597ac91979355265dc7c46
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2025-01-10 09:52:37 +0100

    Finished_ATM_13

--- JavaScript/13_ATM/script.js
+++ JavaScript/13_ATM/script.js
@@ -9,11 +9,41 @@ const readLineAsync = () => {
     return new Promise((resolve) => {
         readline.question("", (userRes) => {
             resolve(userRes);
-            readline.close();
+           
         });
     })
 };
+let balance = 0;
+let runner = true;
 
-console.log("Enter your Name: ")
-let inputOfUser = await readLineAsync();
-console.log("Your Name is " + inputOfUser);
+console.log("Select what you want to do: ")
+console.log("1 Einzahlen");
+console.log("2 Abheben");
+console.log("3 Kontostand");
+console.log("4 Beenden");
+
+while(runner) {
+    let inputOfUser = await readLineAsync();
+    if(inputOfUser == "1"){
+        console.log("Wie viel möchten Sie einzahlen?");
+        let payment = await readLineAsync();
+        balance += parseFloat(payment);
+        console.log("Eingezahlt");
+    }
+    if(inputOfUser == "2"){
+        console.log("Wie viel möchten Sie abheben?");
+        let withdraw = await readLineAsync();
+        balance -= parseFloat(withdraw);
+        console.log("Abgehoben");
+    }
+    if(inputOfUser == "3"){
+        console.log("Ihr Kontostand beträgt: " + balance);
+    }
+    if(inputOfUser == "4"){
+        console.log("Bankomat stoppt");
+        runner = false;
+        readline.close();
+    }
+}
+
+
