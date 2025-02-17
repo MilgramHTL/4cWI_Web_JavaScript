f884bafb2d9db22610114de04c50694fb6648496
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2025-02-17 16:25:33 +0100

    Changed_Letter

--- HTML & JS/01_Zugriff JS HTML/script.js
+++ HTML & JS/01_Zugriff JS HTML/script.js
@@ -4,6 +4,6 @@ function buttonClicked(){
     let num1 = document.getElementById("number1").value;
     let num2 = document.getElementById("number2").value;
 
-    let result = parsefloat(num1) + parsefloat(num2);
+    let result = parseFloat(num1) + parseFloat(num2);
     console.log(result); 
 }
