cd5754f5a08edd3d35ef1cbf4bcdd6c7056a1af3
MilgramHTL <lukas.wagner@student.htldornbirn.at>
2025-02-17 16:23:32 +0100

    Finished_01_Zugriff_auf_HTML

--- HTML & JS/01_Zugriff JS HTML/script.js
+++ HTML & JS/01_Zugriff JS HTML/script.js
@@ -1,1 +1,9 @@
 document.getElementById("content").innerHTML = "Tolle Sache";
+function buttonClicked(){
+    console.log("clicked")
+    let num1 = document.getElementById("number1").value;
+    let num2 = document.getElementById("number2").value;
+
+    let result = parsefloat(num1) + parsefloat(num2);
+    console.log(result); 
+}
